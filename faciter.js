2da39eeba956caa7da8d0dc2e3c4bc2993f29f6a
vagrant <vagrant@precise32.(none)>
2015-02-19 13:02:29 +0000

    Nyt toimii jo hieman paremmin

--- faciter.js
+++ faciter.js
@@ -20,4 +20,31 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+       
 }
+
+//Router init
+  Router.route('/', function () {
+  this.render('MyTemplate');
+});
+
+Router.route('/items', function () {
+  this.render('Items');
+});
+
+Router.route('/items/:_id', function () {
+  var item = Items.findOne({_id: this.params._id});
+  this.render('ShowItem', {data: item});
+});
+
+Router.route('/files/:filename', function () {
+  this.response.end('hi from the server\n');
+}, {where: 'server'});
+
+Router.route('/restful', {where: 'server'})
+  .get(function () {
+    this.response.end('get request\n');
+  })
+  .post(function () {
+    this.response.end('post request\n');
+  });
